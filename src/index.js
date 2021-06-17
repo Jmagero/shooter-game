0634d590c60cb198e300f38c12c3c3527455f5d6
Jmagero <jmagero2017@gmail.com>
2021-06-17 12:16:16 -0500

    Add model, options & credit scenes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import 'phaser';
 import config from './Config/config';
+import Model from './Model';
 import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
 import PreloaderScene from './Scenes/PreloaderScene';
 import TitleScene from './Scenes/TitleScene';
+import CreditsScene from './Scenes/CreditsScene';
+import OptionsScene from './Scenes/OptionsScene';
  
 class Game extends Phaser.Game {
   constructor () {
     super(config);
-    this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Options',OptionsScene);
+    this.scene.add('Credits',CreditsScene);
     this.scene.add('Title', TitleScene);
-    this.scene.add('Game', GameScene);
-    this.scene.start('Game');
+    this.scene.add('Boot', BootScene);
+    const model = new Model();
+    this.globals = { model };
+    this.scene.start('Boot');
   }
 }
  
