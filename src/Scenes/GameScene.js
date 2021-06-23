6d8a7dc72dd21712880d0a6741d6c19c0f644029
Henry Kato <hkato1990@gmail.com>
2021-06-22 21:16:43 -0500

    Add enemies

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,4 +1,6 @@
 import 'phaser';
+import config from '../Config/config';
+import {Entity, Player,CarrierShip,GunShip,ChaserShip, EnemyLaser} from '../Utilities';
  
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -6,8 +8,6 @@ export default class GameScene extends Phaser.Scene {
   }
  
   preload () {
-    // load images
-    this.load.image('logo', 'assets/logo.png');
   }
  
   create () {
@@ -51,6 +51,79 @@ export default class GameScene extends Phaser.Scene {
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group()
+
+    this.time.addEvent({
+      delay: 1000,
+      callback: function() {
+        var enemy = null;
+
+        if (Phaser.Math.Between(0, 10) >= 3) {
+          enemy = new GunShip(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0
+          );
+        }
+        else if (Phaser.Math.Between(0, 10) >= 5) {
+          if (this.getEnemiesByType("ChaserShip").length < 5) {
+    
+            enemy = new ChaserShip(
+              this,
+              Phaser.Math.Between(0, this.game.config.width),
+              0
+            );
+          }
+        }
+        else {
+          enemy = new CarrierShip(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0
+          );
+        }
+    
+        if (enemy !== null) {
+          enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
+          this.enemies.add(enemy);
+        }
+        // var enemy = new GunShip(
+        //   this,
+        //   Phaser.Math.Between(0, this.game.config.width),
+        //   0
+        // );
+        // this.enemies.add(enemy);
+      },
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(this.playerLasers, this.enemies, function(playerLaser, enemy) {
+      if (enemy) {
+        if (enemy.onDestroy !== undefined) {
+          enemy.onDestroy();
+        }
+      
+        enemy.explode(true);
+        playerLaser.destroy();
+      }
+    });
+  }
+
+  getEnemiesByType (type)  {
+    var arr = [];
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+      if (enemy.getData("type") == type) {
+        arr.push(enemy);
+      }
+    }
+    return arr;
+  }
+
   update(){
     this.player.update();
     if (this.keyW.isDown) {
@@ -65,6 +138,34 @@ export default class GameScene extends Phaser.Scene {
     }
     else if (this.keyD.isDown) {
       this.player.moveRight();
+    }
+
+    if (this.keySpace.isDown) {
+      this.player.setData("isShooting", true);
+    }
+    else {
+      this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+      this.player.setData("isShooting", false);
+    }
+
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+
+      enemy.update();
+
+      // if (enemy.x < -enemy.displayWidth ||
+      //   enemy.x > this.game.config.width + enemy.displayWidth ||
+      //   enemy.y < -enemy.displayHeight * 4 ||
+      //   enemy.y > this.game.config.height + enemy.displayHeight) {
+    
+      //   if (enemy) {
+      //     if (enemy.onDestroy !== undefined) {
+      //       enemy.onDestroy();
+      //     }
+    
+      //     enemy.destroy();
+      //   }
+      // }  
     }
   }
 };
