a5365c1fc94b01faf264ed579790fe2cafea0bbb
Henry Kato <hkato1990@gmail.com>
2021-06-22 21:16:14 -0500

    load assets

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -88,6 +88,28 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('box', 'assets/ui/grey_box.png');
     this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
     this.load.audio('bgMusic', ['assets/hit01.wav']);
+    // load more assets
+    this.load.image("sprBg0", "assets/sprBg0.png");
+    this.load.image("sprBg1", "assets/sprBg1.png");
+    this.load.spritesheet("sprExplosion", "assets/sprExplosion.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("sprEnemy0", "assets/sprEnemy0.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.image("sprEnemy1", "assets/sprEnemy1.png");
+    this.load.spritesheet("sprEnemy2", "assets/sprEnemy2.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.image("sprLaserEnemy0", "assets/sprLaserEnemy0.png");
+    this.load.image("sprLaserPlayer", "assets/sprLaserPlayer.png");
+    this.load.spritesheet("sprPlayer", "assets/sprPlayer.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
   }
 
   ready () {
