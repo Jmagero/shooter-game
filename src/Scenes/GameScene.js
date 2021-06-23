e9fafd8a90e03b2e0720074d50c28427d81b91eb
Henry Kato <hkato1990@gmail.com>
2021-06-22 21:05:16 -0500

    Create animations

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -11,6 +11,32 @@ export default class GameScene extends Phaser.Scene {
   }
  
   create () {
-    this.add.image(400, 300, 'logo');
+    this.anims.create({
+      key: "sprEnemy0",
+      frames: this.anims.generateFrameNumbers("sprEnemy0"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprEnemy2",
+      frames: this.anims.generateFrameNumbers("sprEnemy2"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprExplosion",
+      frames: this.anims.generateFrameNumbers("sprExplosion"),
+      frameRate: 20,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: "sprPlayer",
+      frames: this.anims.generateFrameNumbers("sprPlayer"),
+      frameRate: 20,
+      repeat: -1
+    });
   }
 };
